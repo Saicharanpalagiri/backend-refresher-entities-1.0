49c0fe57ecf68201d12deab8fdbfa03773e2cba3
Saicharanpalagiri <charan517214@gmail.com>
2022-12-28 18:36:03 +0530

    Added latestentities

---
+++ src/entities/content-entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { ContentType } from '../../../backend-refresher-1.0-dtos/src/enums/contentType.enum';
+import { EntityBase } from './entityBase';
+import { User } from './user.entity';
+import { UserDto } from '../../../backend-refresher-1.0-dtos/src/dtos/user.dto';
+
+@Entity()
+export class Content extends EntityBase {
+  @Column({
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    nullable: false,
+    default: ContentType.POST,
+  })
+  type: ContentType; /// post and poll only
+
+  @Column()
+  body: string;
+
+  @Column()
+  imageUrls: string;
+
+  @Column()
+  videoUrl: string;
+
+  @ManyToOne(() => User, (user) => user.contents)
+  user: UserDto;
+
+  // userId,
+  // groupId 
+}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,12 +1,27 @@
-import { Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { EntityBase } from './entityBase';
+import { Content } from './content-entity';
+import { ContentDto } from '../../../backend-refresher-1.0-dtos/src/dtos/content.dto';
 
-export class EntityBase {
-  @PrimaryGeneratedColumn()
-  id: number;
+@Entity()
+export class User extends EntityBase {
 
   @Column()
-  creation: Date = new Date();
+  firstname: string;
 
   @Column()
-  modified: Date = new Date();
+  lastname: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phoneNumber: string;
+
+  @Column()
+  password: string;
+
+  @OneToMany(() => Content, (content) => content.user)
+  contents: ContentDto[];
+
 }
